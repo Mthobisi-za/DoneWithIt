041042251013929bc842fa1704cb93838aee7f41
Mthobisi-za <mthobisimtho333@gmail.com>
2022-01-10 20:57:25 +0200

    basic

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text, ImageBackground, Image, Button } from "react-native";
+import { useDimensions } from "@react-native-community/hooks";
+import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
+ 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+ <ImageBackground 
+ style={css.imgBg}
+ source={require('./assets/stockbg.jpg')}>
+   <View style={css.logoHolder}>
+      <Image 
+      width={20}
+      height={20}
+      source={require('./assets/logo.png')}/>
+      <Text style={css.text}>Online Logo Shop</Text>
+   </View>
+   <View style={css.footer}>
+    < Button title="Sign Up" onPress={()=> console.log('user wants to signup')}/>
+    < Button title="Skip" onPress={()=> console.log('user wants to skip and get a logo')}/>
+   </View>
+ </ImageBackground>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const css = StyleSheet.create({
+  imgBg:{
+    width: 360,
+    height: 640
   },
-});
+  logoHolder:{
+    alignItems: "center",
+    backgroundColor: '#ffeee6',
+    width:'100%'
+  },
+  text:{
+    fontSize:20,
+    fontWeight:"900"
+  },
+  footer:{
+    flex: 0.2,
+    top: 350,
+    display: 'flex'
+  }
+})
